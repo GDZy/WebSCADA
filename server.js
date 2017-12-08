e9fceeb6601fe145b91426626d6beb342489a3ff
GD <externalbuffer@gmail.com>
2017-12-08 15:45:12 +0300

    server-setup: node + express c1:L2 - done c1:L3 - start

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ app.get('/users', function(req, res){
     res.send(users);
 })
 
+app.get('/users/:id', function(req, res){
+    var user = users.find(function(user){
+        return user.id === Number(req.params.id);
+    })
+    res.send(user);
+})
+
 app.listen(3012, function(){
     console.log('API app started');
 });
