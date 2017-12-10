d45a541958bc22a6b6bd2b3843d69b7688e9989d
Kronan <kronan@Kronans-MacBook-Air.local>
2017-12-10 20:57:43 +0300

    c1-L3 done. POST, PUT, DELETE c1-L4: work with mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var users = [
     {
@@ -24,8 +28,37 @@ app.get('/users', function(req, res){
 app.get('/users/:id', function(req, res){
     var user = users.find(function(user){
         return user.id === Number(req.params.id);
-    })
+    });
     res.send(user);
+})
+
+app.post('/users', function(req, res){
+    var user = {
+        id: Date.now(),
+        name: req.body.name
+     };
+
+     users.push(user);
+     res.send(user);
+     console.log(req.body);
+})
+
+app.put('/users/:id', function(req, res){
+    var user = users.find(function(user){
+        return user.id === Number(req.params.id);
+    })
+
+    user.name = req.body.name;
+
+    res.sendStatus(200);
+})
+
+app.delete('/users/:id', function(req, res){
+    users = users.filter(function(user){
+        return user.id !== Number(req.params.id);
+    })
+
+    res.sendStatus(200);
 })
 
 app.listen(3012, function(){
