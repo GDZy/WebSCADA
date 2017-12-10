cc6106dc85badea60c91dd9e25a63e7668769471
Kronan <kronan@Kronans-MacBook-Air.local>
2017-12-11 00:29:33 +0300

    c1-L4:done C1-L5: прослойка к BD

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectId;
 
 var app = express();
+var db;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,25 +25,47 @@ app.get('/', function(req, res){
 });
 
 app.get('/users', function(req, res){
-    res.send(users);
+    db.collection('users').find().toArray(function (err, docs) {
+        if (err) {
+            console.log(err);
+            return sendStatus(500);
+        }
+        res.send(docs);
+    });
 })
 
 app.get('/users/:id', function(req, res){
-    var user = users.find(function(user){
-        return user.id === Number(req.params.id);
-    });
-    res.send(user);
+    db.collection('users').findOne( { _id: ObjectID(req.params.id) }, function (err, doc) {
+        if (err) {
+            console.log(err);
+            return sendStatus(500);
+        }
+        res.send(doc);
+    })
+    
+ //   var user = users.find(function(user){
+ //       return user.id === Number(req.params.id);
+ //   });
+ //   res.send(user);
 })
 
-app.post('/users', function(req, res){
+app.post('/users', function (req, res){
     var user = {
-        id: Date.now(),
+        //id: Date.now(),
         name: req.body.name
      };
 
-     users.push(user);
-     res.send(user);
      console.log(req.body);
+     
+     db.collection('users').insert(user, function (err, result) {
+     
+         if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+         }
+
+         res.send(user);
+     })
 })
 
 app.put('/users/:id', function(req, res){
@@ -61,6 +86,12 @@ app.delete('/users/:id', function(req, res){
     res.sendStatus(200);
 })
 
-app.listen(3012, function(){
-    console.log('API app started');
-});
+MongoClient.connect('mongodb://localhost:27017', function(err, database){
+    if (err) {
+        return console.log(err);
+    }
+    db = database.db('dbCount');
+    app.listen(3012, function() {
+        console.log('API app started');
+    });
+})
